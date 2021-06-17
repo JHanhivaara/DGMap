c28e6f5244049cb534b3246672ced8e01a7f56b7
JHanhivaara <77466940+JHanhivaara@users.noreply.github.com>
2021-06-17 22:32:45 +0300

    Update data.js
    Cleaning up temp test data.

--- src/data/data.js
+++ src/data/data.js
@@ -1,21 +1,21 @@
 export const locations = [{
     index: 1,
-    title: 'POHJONENE SAATANA',
+    title: 'Northern Finland Course',
     latitude: 65.78893235131767,
     longitude: 27.72059811743988,
-    description: 'doggy:!""#'
+    description: 'North location, Disc Golf'
 },
 {
     index: 2,
-    title: 'Harjun rata',
+    title: 'Harju Course',
     latitude: 62.2460517,
     longitude: 25.7402853,
-    description: 'dog45454585945748'
+    description: 'Harju location, Disc Golf'
 },
 {
     index: 3,
-    title: 'keskisuomi K4O4435435435355',
+    title: 'Central Finland Course',
     latitude: 62.29382872162948,
     longitude: 25.20599265136716,
-    description: 'k,eskkisuomi'
+    description: 'Central Finald, Disc Golf'
 }]
